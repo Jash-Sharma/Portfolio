6f5f06b6f90684502fd5a3613df9f89c07c99f12
cruelcaretaker <jashsharma1310@gmail.com>
2023-12-05 17:47:33 +0530

    loading ts-particles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Project from "./components/Project";
 import Skill from "./components/Skill";
+import Particle from './components/Particle'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Skill />
       <Project />
       <Contact />
+      <Particle />
     </div>
   );
 }
